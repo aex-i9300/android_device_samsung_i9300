38a5ff15ee9df25bb8485dbb69596b3584713d3a
Seden Ecem <ttbpccorporation@outlook.com>
2019-06-07 14:33:21 +0000

    Changes for AospExtended Building
    Signed-off-by: Seden Ecem <ttbpccorporation@outlook.com>

--- aosp_i9300.mk
+++ aosp_i9300.mk
@@ -1,34 +1,20 @@
-#
-# Copyright (C) 2019 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Release name
 PRODUCT_RELEASE_NAME := i9300
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
+$(call inherit-product, device/samsung/i9300/i9300.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/i9300/device.mk)
-$(call inherit-product, device/samsung/smdk4412-common/common.mk)
-
-# Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
 # Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
@@ -36,8 +22,6 @@ PRODUCT_NAME := aosp_i9300
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo aosp_i9300-userdebug
+add_lunch_combo aosp_i9300-eng
